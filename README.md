ff6e9dc54493690c2b7ab7cfdb8772fc7200b210
Evgeny Shestakov <evgeny.shestakov@gmail.com>
2017-08-01 12:40:16 +0000

    first implementation

--- README.md
+++ README.md
@@ -1,7 +1,99 @@
+# Uses
+- sys
+- re
+
+
 # Password Strength Calculator
 
-[TODO. There will be project description]
+Calculate password strength and return score: from 1 (very bad password) to 10 (very good password) uses rules:
 
+- the use of both upper-case and lower-case letters (case sensitivity)
+- inclusion of one or more numerical digits
+- inclusion of special characters, such as @, #, $
+- prohibition of words found in a password blacklist
+- prohibition of words found in the user's personal information
+- prohibition of use of company name or an abbreviation
+- prohibition of passwords that match the format of calendar dates, license plate numbers, telephone numbers, or other common numbers
+
+functions:
+
+- load_file(filepath):
+  load file as text
+        
+        
+- convert_text_to_words(raw_text):
+  convert text to separete words dictionary
+
+
+- check_special_characters(password):
+   check special characters as "$", "#", "@", "!" in password string
+    
+    
+- check_substrings_containing(password, user_info =[]):
+  check password is containing some substrings     
+
+ 
+- check_digits_and_strings(password):
+  check digits and char symbols containing
+
+
+- check_upper_lower(password):
+    check upper and lower symbols containing
+
+- check_standard_formats(password):
+  check password is in some standard formats
+
+
+- get_password_strength(password, blacklist = [], 
+                        user_info = [], abbreviations = [])
+  main function for calculating score  
+
+# How to use
+
+launch: ./python password_strength.py 'some password' [blacklist file] [user personal info file] [abbreviations file]
+
+```#!bash
+
+python password_strength.py 'bad' blacklist.txt userpersonal.txt abbreviations.txt                                                                                 
+password strength is 1 of 10
+
+
+python password_strength.py 'Better' blacklist.txt userpersonal.txt abbreviations.txt                                                                              
+password strength is 6 of 10
+
+
+python password_strength.py 'VeryGood!1' blacklist.txt userpersonal.txt abbreviations.txt                                                                          
+password strength is 10 of 10                         
+
+```
+
+blacklist file for exmple "blacklist.txt":
+
+test
+12345
+54321
+1234567890
+0987654321
+poiuy
+qwerty
+asdf
+zxcvb
+password
+password1
+Password1
+
+user personal info file for exmple "userpersonal.txt":
+
+name
+surname
+email@mail.ru
+
+abbreviations file for exmple "abbreviations.txt":
+
+someCompany1
+SomeNane2
+Test&Company25
+ 
 # Project Goals
 
 The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
--- password_strength.py
+++ password_strength.py
@@ -1,8 +1,108 @@
+import sys
+import re
 
 
-def get_password_strength(password):
-    pass
+def load_file(filepath):
+    try:
+        with open(filepath, 'r') as file_handler:              
+            return file_handler.read()
+    except FileNotFoundError:
+        print('file {0} not found'.format(filepath))
+        sys.exit(1)
+        
+        
+def convert_text_to_words(raw_text):
+    return [word for word in raw_text.split('\n') if len(word) > 0]
+
+
+def check_special_characters(password):
+    return bool(re.search('[$,#,@,\!]', password))
+    
+    
+def check_substrings_containing(password, user_info =[]):
+    for info in user_info:
+        if password in info:
+            return True
+    return False    
+
+ 
+def check_digits_and_strings(password):
+    return (bool(re.search('\d+', password)) and
+        bool(re.search('\w+', password)))
+
+
+def check_upper_lower(password):
+    if password.isnumeric():
+        return False
+    elif bool(re.search('\d+', password)):
+        return True
+    return not password.islower() and not password.isupper()
+    
+
+def check_standard_formats(password):
+    expressions = ['^[\+]?[a-z0-9]{3}-[a-z0-9]{3}-[a-z0-9]{4}$',
+                '^[\w\.\+\-]+\@[\w]+\.[a-z]{2,3}$']
+    for expr in expressions:
+        if bool(re.search(expr, password)):
+            return True    
+    return False
+
+
+def get_password_strength(password, blacklist = [], 
+                        user_info = [], abbreviations = []):
+    strength = 1
+    if len(password) <= 3 or password in blacklist:
+        return strength;
+    elif len(password) > 8:
+        strength += 2
+    elif len(password) > 6:
+        strength += 1    
+        
+    if check_upper_lower(password): 
+        strength += 2
+    
+    if check_digits_and_strings(password):
+        strength += 1
+    
+    if check_special_characters(password):
+        strength += 1
+        
+    # check personal info
+    if check_substrings_containing(password, user_info):
+        strength = (1 if strength < 4 else strength - 3)
+    else:
+        strength += 1
+        
+    # check abbreviations
+    if check_substrings_containing(password, abbreviations):
+        strength = (1 if strength < 3 else strength - 2)
+    else:
+        strength += 1
+        
+    if check_standard_formats(password):
+        strength = (1 if strength < 3 else strength - 2)
+    else:
+        strength += 1
+    
+    return strength 
 
 
 if __name__ == '__main__':
-    pass
+    if len(sys.argv) > 1:
+        blacklist = ([] if len(sys.argv) <= 2 else 
+            convert_text_to_words(load_file(sys.argv[2])))
+            
+        user_info = ([] if len(sys.argv) <= 3 else 
+            convert_text_to_words(load_file(sys.argv[3])))
+            
+        abbreviations = ([] if len(sys.argv) <= 4 else 
+            convert_text_to_words(load_file(sys.argv[4])))
+   
+        password_strength = get_password_strength(sys.argv[1], blacklist,
+                                                user_info, abbreviations)
+        print('password strength is {0} of 10'.format(password_strength))
+        sys.exit(0)
+    else:
+        print('please add password as argument: ' + 
+            './python password_strength.py some_password')
+        sys.exit(1)    
